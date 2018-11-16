ec3649c700f4dc0d32239fed40cf46b0b5731e37
drocca <roccad@gmail.com>
2018-11-16 20:27:50 +0100

    Now parse_pwin.py provides the tools to read the pw.x input file, modify it, and rewrite it to another file. It will be used to change the k-points in the input but is much more general than that

--- parse_pwin.py
+++ parse_pwin.py
@@ -1,20 +1,41 @@
 import numpy as np
 import f90nml
+import sys
 
 def parse_pwin_nmlist(filein):
-
+    """
+    This function uses the f90nml library to parse the namelists in the input of pw.x
+    For example, nml["system"]["ecutwfc"] will contain the cutoff
+    """
     nml = f90nml.read(filein)
 
     return nml
 
 class cards_pwin():
+    """
+    Class defined to store the cards in the pw.x input 
+    """
 
     def __init__(self):
 
-        self.atomic_species={}
+        # Example: .atomic_species["Si"]["mass"] will give the atomic mass of Si in the file
+        # .atomic_species["Si"]["pseudopot"] the pseudopotential
+        self.atomic_species={} 
+
+        # Example: .atomic_positions[2]["element"] will give the name of the second element in the list of atoms
+        # .atomic_positions[2]["position"] its position (numpy array)
         self.atomic_positions={}
+
+        # If cell_parameters is present it will contain a list with 3 numpy arrays (the 3 cell parameters)
         self.cell_parameters=[]
-        self.k_points=[]
+
+        # If K_POINTS automatic it stores a 6 component 1D array with grid and shift 
+        self.k_points_auto=np.asarray([1, 1, 1, 0, 0, 0], dtype="int")
+
+        # If K_POINTS is not automatic or gamma
+        # this list will contain nk 4 component 1D array with k-point and weight  
+        self.k_points_list=[]
+
         self.unit_pos = "alat"
         self.unit_cellp = "bohr"
         self.ktype = "tpiba"
@@ -22,26 +43,32 @@ class cards_pwin():
 
     def read(self, pwin, ntyp, nat):
 
+        # Open pw.x input file
         with open(pwin, "r") as f:
-            lines = f.readlines()
+            lines_tot = f.readlines()
+
+        # Eliminates empty lines
+        lines= []
+        for i in range(0, len(lines_tot)):
+            if not lines_tot[i].strip():
+                continue
+            else:
+                lines.append(lines_tot[i].strip())
+            
 
         for i in range(0, len(lines)):
 
-            if (lines[i].strip()=="ATOMIC_SPECIES"): 
+            if ("ATOMIC_SPECIES" in lines[i]): 
                 
-                k=0                
-                for j in range(i+1, len(lines)):
-                    if not lines[j].strip():
-                        continue
-                    else:
-                        k+=1
-                        ll = lines[j].split() 
-                        self.atomic_species[ll[0]] = {"mass":ll[1], "pseudopot":ll[2]}
-                        if (k==ntyp):
-                            break
+                # Creates a dictionary with atomic species
+                # Example: {"Si":{"mass": "28.0855", "pseudopot": "Si.UPF"}}
+                for j in range(i+1, i+1+ntyp):
+                    ll = lines[j].split() 
+                    self.atomic_species[ll[0]] = {"mass":ll[1], "pseudopot":ll[2]}
 
             if ("ATOMIC_POSITIONS" in lines[i]):
 
+                # Determines the units; is none use consider alat 
                 ap_optionlist = ["alat", "bohr", "angstrom", "crystal_sg", "crystal"]
                 apu = filter(lambda x: x in lines[i], ap_optionlist)
                 if not apu:
@@ -49,19 +76,15 @@ class cards_pwin():
                 else:
                     self.unit_pos = apu[0]
 
-                k=0
-                for j in range(i+1, len(lines)):
-                    if not lines[j].strip():
-                        continue
-                    else:
-                        k+=1
-                        ll = lines[j].split()
-                        self.atomic_positions[k] = {"element":ll[0], "position":np.asarray([ll[1], ll[2], ll[3]],dtype="float")}
-                        if (k==nat):
-                            break
+                # Creates a dictionary with atomic positions and type
+                # For example for the second atom in the list we could have {2: "element":"Si", "position":[0.25 0.25 0.25]}
+                for counter, j in enumerate(range(i+1, i+1+nat)):
+                    ll = lines[j].split()
+                    self.atomic_positions[counter+1] = {"element":ll[0], "position":np.asarray([ll[1], ll[2], ll[3]],dtype="float")}
 
             if ("CELL_PARAMETERS" in lines[i]):
 
+                # Determines the units
                 cp_optionlist = ["alat", "bohr", "angstrom"]
                 cpu = filter(lambda x: x in lines[i], cp_optionlist) 
                 if not cpu:
@@ -69,18 +92,14 @@ class cards_pwin():
                 else:
                     self.unit_cellp = cpu[0]
 
-                k=0
-                for j in range(i+1, len(lines)):
-                    if not lines[j].strip():
-                        continue
-                    else:
-                        k+=1
-                        self.cell_parameters.append(np.asarray(lines[j].split()))
-                        if (k==3):
-                            break
+                # Set up a list with 3 vectors corresponding to the cell parameters
+                for j in range(i+1, i+4):
+                    self.cell_parameters.append(np.asarray(lines[j].split(),dtype="float"))
 
             if ("K_POINTS" in lines[i]):
 
+                # Determines if k-points is automatic, gamma, or explicit list of k-points 
+                # If list of k-points it determines the units
                 k_optionlist = ["automatic", "gamma", "tpiba_b" , "tpiba_c", "tpiba", "crystal_b", "crystal_c", "crystal"]
                 kt = filter(lambda x: x in lines[i], k_optionlist)
                 if not kt:
@@ -89,56 +108,77 @@ class cards_pwin():
                     self.ktype = kt[0]
 
                 if (self.ktype == "automatic"):
-                    for j in range(i+1, len(lines)):
-                        if not lines[j].strip():
-                            continue
-                        else: 
-                            self.k_points = np.asarray(lines[j].split(),dtype="int") 
-                            break
+
+                    self.k_points_auto = np.asarray(lines[i+1].split(),dtype="int") 
+
                 elif (self.ktype != "gamma") and (self.ktype != "automatic"):
-                    for j in range(i+1, len(lines)):
-                        if not lines[j].strip():
-                            continue
-                        else:
-                            self.nk = int(lines[j].split()[0])
-                            l_nk = j
-                            break
+                    self.nk = int(lines[i+1].split()[0])
                     
-                    self.k_points = [] 
                     k=0
-                    for j in range(l_nk+1, len(lines)):
-                        if not lines[j].strip():
-                            continue
-                        else:
-                            k+=1
-                            self.k_points.append(np.asarray(lines[j].split(),dtype="float"))
-                            if (k==self.nk):
-                                break
+                    for j in range(i+2, i+2+self.nk):
+                        self.k_points_list.append(np.asarray(lines[j].split(),dtype="float"))
 
-#            if ("OCCUPATIONS" in lines[i]):
+            if ("OCCUPATIONS" in lines[i]):
+                sys.exit("Parsing of OCCUPATIONS not implemented")
 
 
-#            if ("CONSTRAINTS" in lines[i]):
+            if ("CONSTRAINTS" in lines[i]):
+                sys.exit("Parsing of CONSTRAINTS not implemented")
 
-
-#            if ("ATOMIC_FORCES" in lines[i]):
-
+            if ("ATOMIC_FORCES" in lines[i]):
+                sys.exit("Parsing of ATOMIC_FORCES not implemented")
 
 
         return 
 
     def write(self,fout):
         """
-        Writes the cards in the output file fout
+        Writes the cards in a new input file called fout
         """
-         
+
+        fout.write("ATOMIC_SPECIES"+"\n")
+        for key in self.atomic_species:
+            fout.write(key+" "+self.atomic_species[key]["mass"]+" "+self.atomic_species[key]["pseudopot"]+"\n")
+
+        fout.write("ATOMIC_POSITIONS"+" "+"{"+self.unit_pos+"}"+"\n")
+        for i in range(1, len(self.atomic_positions)+1): 
+            pos = self.atomic_positions[i]["position"]
+            fout.write(self.atomic_positions[i]["element"]+" "+('%12.8f %12.8f %12.8f'%tuple(pos))+"\n")
+
+        if len(self.cell_parameters)>0: 
+            fout.write("CELL_PARAMETERS"+" "+"{"+self.unit_cellp+"}"+"\n")
+            fout.write(('%12.8f %12.8f %12.8f'%tuple(self.cell_parameters[0])).strip()+"\n")
+            fout.write(('%12.8f %12.8f %12.8f'%tuple(self.cell_parameters[1])).strip()+"\n")
+            fout.write(('%12.8f %12.8f %12.8f'%tuple(self.cell_parameters[2])).strip()+"\n")
+
+        fout.write("K_POINTS"+" "+"{"+self.ktype+"}"+"\n")
+        
+        if (self.ktype == "automatic"):
+            grd = [str(x) for x in self.k_points_auto]
+            fout.write(grd[0]+" "+grd[1]+" "+grd[2]+" "+grd[3]+" "+grd[4]+" "+grd[5])
+        elif (self.ktype != "gamma") and (self.ktype != "automatic"):
+            fout.write(str(self.nk)+"\n")
+            for i in range(0, self.nk):  
+                fout.write(('%12.8f %12.8f %12.8f %12.8f'%tuple(self.k_points_list[i])).strip()+"\n")
+
         return 
 
 if __name__ == '__main__':
-    cd = cards_pwin()
-    cd.read('scf.in',1,2)
-    print cd.atomic_species
-    print cd.atomic_positions
-    print cd.cell_parameters
-    print cd.nk
-    print cd.k_points
+
+    # pw.x input file
+    name_pwin = 'scf.in'
+
+    # parsing namelists
+    pw_nml = parse_pwin_nmlist(name_pwin)
+
+    # parsing cards
+    n_typ = pw_nml["system"]["ntyp"]
+    n_at = pw_nml["system"]["nat"]
+
+    pw_cards = cards_pwin()
+    pw_cards.read(name_pwin,n_typ,n_at) 
+ 
+    # writing back to a file
+    with open("scf_parsed.in", 'w') as fout:
+        pw_nml.write(fout)
+        pw_cards.write(fout) 
